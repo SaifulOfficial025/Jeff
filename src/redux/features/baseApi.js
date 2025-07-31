5477018a3f882b5260f7a92ca845ed089fd273fd
Md Saiful Islam Rimon <saifulofficial025@gmail.com>
2025-07-31 11:18:52 +0600

    url changed

--- src/redux/features/baseApi.js
+++ src/redux/features/baseApi.js
@@ -7,7 +7,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({ 
-        baseUrl: 'https://clicking-numbers-refined-talked.trycloudflare.com',
+        baseUrl: 'https://trick-offered-certificate-wy.trycloudflare.com',
 
           prepareHeaders: (headers) => {
             const token = localStorage.getItem("access_token");
