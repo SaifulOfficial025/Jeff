4a4ca55c3695576f67f2554242b5fae29e9d2a7e
Md Saiful Islam Rimon <saifulofficial025@gmail.com>
2025-08-25 17:49:56 +0600

    adsfasdf

--- src/redux/features/employeeSlice.js
+++ src/redux/features/employeeSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const baseUrl =  'https://commit-orientation-northeast-emacs.trycloudflare.com';
+const baseUrl =  'https://retailer-commander-cell-deposit.trycloudflare.com';
 
 export const fetchEmployeeStats = createAsyncThunk(
   'employee/fetchStats',
